bbc9d026d8c9ae8ed44a87a79ab7807a8f7e40b2
PedroMMarinho <pmiguel.marinho@gmail.com>
2026-07-07 13:13:42 +0100

    Achieved version of module working

--- configs/.config/waybar/scripts/playerinfo.sh
+++ configs/.config/waybar/scripts/playerinfo.sh
@@ -5,7 +5,6 @@ if ! playerctl status >/dev/null 2>&1; then
     exit 0
 fi
 
-player_name=$(playerctl metadata --format '{{playerName}}')
 artist_title=$(playerctl metadata --format '{{artist}} - {{title}}')
 
 artist_title_escaped=${artist_title//\"/\\\"}
@@ -17,4 +16,4 @@ if [ ${#bar_text} -gt $maxlength ]; then
     bar_text="${bar_text:0:$maxlength-3}..."
 fi
 
-printf '{"text": "%s", "tooltip": "%s : %s"}\n' "$bar_text" "$player_name" "$artist_title_escaped"
+printf '{"text": "%s", "tooltip": "%s"}\n' "$bar_text" "$artist_title_escaped"
