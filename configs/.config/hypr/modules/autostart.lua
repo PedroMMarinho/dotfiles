47d58549bc5aa141899a9a18a8e3cb8051c7c7cd
PedroMMarinho <pmiguel.marinho@gmail.com>
2026-08-19 01:44:30 +0100

    Added scan timeout

--- configs/.config/hypr/modules/autostart.lua
+++ configs/.config/hypr/modules/autostart.lua
@@ -21,5 +21,5 @@ hl.on("hyprland.start", function ()
   hl.exec_cmd("awww-daemon") 
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
   hl.exec_cmd("qs")
-  hl.exec_cmd("hyprpm reload") -- plugin manager
+  --hl.exec_cmd("hyprpm reload") -- plugin manager
 end)
--- configs/.config/hypr/modules/monitors.lua
+++ configs/.config/hypr/modules/monitors.lua
@@ -11,18 +11,19 @@
 
 -- Might not put best refresh rate
 hl.monitor({
-    output   = "HDMI-A-1",
-    mode     = "preferred",
-    position = "0x0",
-    scale    = 1,
+  output = "eDP-2",
+  mode = "1920x1080@144",
+  position = "0x0",
+  scale = 1,
 })
 
 hl.monitor({
-  output = "eDP-2",
-  mode = "1920x1080@144",
-  position = "auto",
-  scale = 1,
+    output   = "HDMI-A-1",
+    mode     = "preferred",
+    position = "auto",
+    scale    = 1,
 })
+
 
 hl.monitor({
     output   = "DP-2",
